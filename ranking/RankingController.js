134c673b5a6953f432abe0fd9b9d645a3d0c1a25
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2022-06-04 01:08:15 -0300

    Add files via upload

--- ranking/RankingController.js
+++ ranking/RankingController.js
@@ -2,7 +2,23 @@ const express = require('express');
 const router = express.Router();
 const Ranking = require('./Ranking.js');
 
-router.get('/ranking/question', (req,res)=>{
-	res.send('AAA');
+router.get('/ranking/new', (req,res)=>{
+	res.render('index.ejs');
+});
+
+router.post('/ranking/save', (req,res)=>{
+	let name = req.body.name;
+	let email = req.body.email;
+	if(!name || !email){
+		res.redirect('/ranking/new');
+	}else{
+		Ranking.create({
+			name: name,
+			email: email,
+			score: 0
+		}).then(()=>{
+			res.redirect('/ranking/new');
+		})
+	}
 });
 module.exports = router;
